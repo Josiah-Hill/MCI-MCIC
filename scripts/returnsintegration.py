8a77affc51b17c7e40b2adf7530a8355e0295dc4
Josiah-Hill <114089042+Josiah-Hill@users.noreply.github.com>
2025-01-24 15:34:07 -0500

    Handle yfinance data correctly

--- scripts/returnsintegration.py
+++ scripts/returnsintegration.py
@@ -2,14 +2,17 @@
 
 import pandas as pd
 
+
 def add_stock_info(pairs, returns_file, nyse_returns_file):
     """Add stock information from returns files to a DataFrame of ticker pairs"""
     # Read stock data from returns file
     stock_data = pd.read_csv(returns_file, usecols=["TICKER", "date", "VOL", "RET"])
 
     # Read NYSE returns data
-    nyse_data = pd.read_csv(nyse_returns_file, usecols=["TICKER", "date", "RET"])
-    nyse_data.rename(columns={"RET": "NYSE_RET"}, inplace=True)
+    nyse_data = pd.read_csv(nyse_returns_file, usecols=["Date", "Daily Returns"])
+    nyse_data.rename(
+        columns={"Daily Returns": "NYSE_RET", "Date": "date"}, inplace=True
+    )
 
     # Merge stock data for T1
     data_t1 = pairs.merge(stock_data, left_on="T1", right_on="TICKER")
@@ -24,7 +27,10 @@ def add_stock_info(pairs, returns_file, nyse_returns_file):
     # Merge T1 and T2 data
     data = pd.merge(data_t1, data_t2, on=["T1", "T2", "date"])
 
+    # Convert dates to datetime
+    data["date"] = pd.to_datetime(data["date"]).dt.tz_localize("UTC")
+    nyse_data["date"] = pd.to_datetime(nyse_data["date"])
     # Merge with NYSE returns
-    final_data = data.merge(nyse_data, on=["TICKER", "date"], how="left")
+    final_data = data.merge(nyse_data, on=["date"], how="left")
 
     return final_data
